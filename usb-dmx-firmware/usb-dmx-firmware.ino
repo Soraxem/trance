c4cd6138dc2e6f183dd97c8b2f3e024406e6e0e8
Samuel Hafen <samuel.hafen@aol.com>
2025-10-02 19:31:52 +0200

    adding dmx output, removing pwm outputs

--- usb-dmx-firmware/usb-dmx-firmware.ino
+++ usb-dmx-firmware/usb-dmx-firmware.ino
@@ -1,26 +1,49 @@
+/*
+
+  Boards: esp32 v3.0.4
+  Dependencies: esp_dmx v4.1.0
+*/
+
+#include <Arduino.h>
+#include <esp_dmx.h>
+
 #include <trance.h>
+
+int transmitPin = 4;
+int receivePin = 1;
+int enablePin = 3;
+
+dmx_port_t dmxPort = 1;
+
+uint8_t data[513];
 
 // Prototype because compiler sad
 void callback(uint8_t test[]);
 
-
 void setup() {
   // setup Trance
-  Trance.begin(3, callback);
+  delay(2000);
 
-  // setup PWM LED Pins
-  pinMode(1, OUTPUT); // red
-  pinMode(2, OUTPUT); // green
-  pinMode(4, OUTPUT); // blue
+  Trance.begin(512, callback, 0);
+
+  // Setup DMX Driver
+  dmx_config_t config = DMX_CONFIG_DEFAULT;
+  dmx_personality_t personalities[] = {};
+  int personality_count = 0;
+  dmx_driver_install(dmxPort, &config, personalities, personality_count);
+  dmx_set_pin(dmxPort, transmitPin, receivePin, enablePin);
 }
 
 void loop() {
   Trance.handle();
+
+  dmx_write(dmxPort, data, DMX_PACKET_SIZE);
+  dmx_send_num(dmxPort, DMX_PACKET_SIZE);
+  dmx_wait_sent(dmxPort, DMX_TIMEOUT_TICK);
+  delay(5);  //allow the cpu to switch to other tasks
 }
 
 // recieves data from Trance
 void callback(uint8_t channels[]) {
-  analogWrite(1, channels[0]);
-  analogWrite(2, channels[1]);
-  analogWrite(4, channels[2]);
+  memcpy(data, channels, sizeof(data));
 }
